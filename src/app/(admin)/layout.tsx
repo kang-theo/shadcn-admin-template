cf477397e8e9c7b6cfbc6aa63422a1bef5f185f2
kang-theo <theo.cy.kang@gmail.com>
2024-02-03 20:23:02 +0800

    Add navbar and profile dropdown menu, and introduce nested layout

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { MainNav } from "@/components/layout/navs/MainNav";
+import { ProfileNav } from "@/components/layout/navs/ProfileNav";
+
+function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    // Tailwind Mobile Responsive
+    <div className='hidden flex-col md:flex'>
+      <div className='border-b'>
+        <div className='flex h-16 items-center px-4'>
+          <p>Logo</p>
+          <MainNav className='mx-6' />
+          <div className='ml-auto flex items-center space-x-4'>
+            <ProfileNav />
+          </div>
+        </div>
+      </div>
+
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default AdminLayout;
---
+++ src/components/layout/navs/ProfileNav.tsx
@@ -0,0 +1,73 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { auth } from "@/lib/auth";
+import { Session } from "next-auth";
+import { signOutAction } from "@/app/actions/auth";
+
+export async function ProfileNav() {
+  const session: Session | null = await auth();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+          <Avatar className='h-8 w-8'>
+            <AvatarImage src='/avatars/01.png' alt='@shadcn' />
+            <AvatarFallback>SC</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56' align='end' forceMount>
+        <DropdownMenuLabel className='font-normal'>
+          <div className='flex flex-col space-y-1'>
+            <p className='text-sm font-medium leading-none'>
+              {session?.user?.name}
+            </p>
+            <p className='text-xs leading-none text-muted-foreground'>
+              {session?.user?.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>New Team</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <form action={signOutAction}>
+            <Button
+              type='submit'
+              variant='ghost'
+              className='h-5 px-0 hover:bg-none font-normal'
+            >
+              Log out
+            </Button>
+          </form>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
