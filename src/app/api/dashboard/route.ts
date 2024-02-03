6b3e9338d32f6fd2027a230dc974f9407208c76d
kang-theo <theo.cy.kang@gmail.com>
2024-02-03 20:29:02 +0800

    Set route for dashboard to get ready for fetching data from database

---
+++ src/app/api/dashboard/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+
+// respond to GET requests
+export const GET = async (req: NextRequest) => {
+  // TODO: get real data from database instead of mocking
+};
