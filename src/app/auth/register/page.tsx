5f3232f3a222935e96023d2c689d7ceb56d0a0c2
kang-theo <theo.cy.kang@gmail.com>
2024-02-03 13:03:54 +0800

    Add register page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,32 @@
+import { Metadata } from "next";
+import React from "react";
+import TermAndPrivacy from "@/components/common/TermAndPrivacy";
+
+import { UserAuthForm } from "@/components/auth/AuthForm";
+
+export const metadata: Metadata = {
+  title: "Shadcn admin starter authentication",
+  description:
+    "Authentication forms built using the shadcn, tailwind, next.js etc.",
+};
+
+function Register() {
+  return (
+    <div className='container h-full flex-col items-center justify-center md:grid lg:max-w-none p-8'>
+      <div className='lg:p-8'>
+        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+          <div className='flex flex-col space-y-2 text-center'>
+            <h1 className='text-2xl font-semibold tracking-tight'>Sign Up</h1>
+            <p className='text-sm text-muted-foreground'>
+              Enter your username and password below
+            </p>
+          </div>
+          <UserAuthForm type='signup' />
+          <TermAndPrivacy />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
