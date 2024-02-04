ba521f9c042e59c7b358fcb8a14046b29a5ef0f3
kang-theo <theo.cy.kang@gmail.com>
2024-02-04 15:43:30 +0800

    Solve the problem of two nav bars

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import AdminLayout from "@/app/(admin)/layout"
 import { Session } from 'next-auth';
 import { auth } from "@/lib/auth";
 import { Button } from '@/components/ui/button';
@@ -9,7 +8,7 @@ async function Dashboard() {
   const session: Session | null = await auth();
 
   return (
-    <AdminLayout>
+    <div>
       {/* page related components put here */}
       <div className='flex items-center justify-between space-y-2'>
         <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
@@ -18,7 +17,7 @@ async function Dashboard() {
           <Button>Download</Button>
         </div>
       </div>
-    </AdminLayout>
+    </div>
   )
 }
 
