4b14ed0a4ddd1b52c0fe8d95b9aabb7b2b80a172
kang-theo <theo.cy.kang@gmail.com>
2024-03-26 15:46:34 +0800

    Prerendering page error

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -3,4 +3,15 @@ import { NextRequest } from "next/server";
 // respond to GET requests
 export const GET = async (req: NextRequest) => {
   // TODO: get real data from database instead of mocking
+  return new Response(
+    JSON.stringify({
+      message: "Hello, World!",
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
